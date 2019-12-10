e9a63de710f262f310e7a43aab3a5216b5251867
Micah Levine <micah.levine@outlook.com>
2019-12-10 14:32:29 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,12 +3,17 @@ class Dog
   attr_accessor :name, :breed
   attr_reader :id 
 
-  def initialize(id=nil, name, breed)
-    @id = id
-    @name = name
-    @breed = breed
-  end
+  #def initialize(id=nil, name, breed)
+  #  @id = id
+  #  @name = name
+  #  @breed = breed
+  #end
 
+  def initialize(id=nil, name, breed)
+      @id = id
+      @name = name
+      @breed = breed
+    end
 
 
 
