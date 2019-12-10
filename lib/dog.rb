481dd92f51792cb1ce68807cc9448e49884d1b96
Micah Levine <micah.levine@outlook.com>
2019-12-10 20:27:55 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -66,4 +66,17 @@ class Dog
     dog
   end
 
+  def find_by_id(id)
+    sql =<<-SQL
+      SELECT *
+      FROM dogs 
+      WHERE id = ?
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql, id).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+  
 end
